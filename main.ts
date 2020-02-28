d59d9961ebe4005da0862936de9d84cfae7eb217
chatan000 <58610792+chatan000@users.noreply.github.com>
2020-02-28 13:29:50 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -15,42 +15,42 @@ let favoritefood = sprites.create(img`
 . . . . . . . . . . b 4 5 5 d 4 4 4 3 3 3 3 4 4 4 b b . . . . . 
 . . . . . . . . . . 4 d 5 5 5 5 d 4 4 4 3 3 3 4 4 4 b b . . . . 
 . . . . . . . . . 4 5 5 5 5 5 5 d d 4 b 4 4 4 3 4 4 4 b e . . . 
-. . . . . . . . b d 5 5 5 5 d 5 5 5 d 4 b b 4 4 4 4 4 4 e e . . 
-. . . . . . . . b 5 5 d d d 6 6 5 5 5 5 b b 4 b 4 4 4 4 4 e e . 
-. . . . . . . b d 5 5 5 5 6 7 7 6 5 5 5 5 4 e b b 4 4 4 4 4 e b 
-. . . . . . . 4 3 2 2 2 5 5 8 8 5 5 5 5 5 d 4 e e b 4 4 b 4 e e 
-. . . . . . b 4 2 2 3 3 2 d d d 5 5 5 2 2 2 d 4 e e b b b 4 b e 
-. . . . . . b e 2 2 2 3 2 e d 5 5 5 2 2 3 3 2 d b 4 e b b b b e 
-. . . . . b d e 2 2 2 2 2 e 5 5 5 e 2 2 2 3 2 e d d 4 b b b b e 
-. . . . . b 5 2 e 2 2 2 e 3 5 5 5 e 2 2 2 2 2 e d d d 4 e b e e 
-. . . . b d 5 5 2 e e e 3 5 5 5 5 2 e 2 2 2 e 3 5 5 5 4 4 e e e 
-. . . . 6 6 5 d d 5 5 5 5 5 5 5 5 5 2 e e e 3 5 5 d d 4 d e e e 
-. . . 6 7 7 6 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 1 5 5 4 d 4 e e 
-. . . 4 8 8 5 2 2 2 5 5 5 d d d d 6 6 5 5 5 5 1 5 4 4 d 4 e e e 
-. . b 4 5 5 2 2 3 3 2 5 5 5 d d 6 7 7 6 1 1 d 4 4 4 d d e e e . 
-. . 4 d 5 2 2 2 2 3 2 e 5 5 5 5 5 8 8 1 d 4 4 d d d 4 e e e . . 
-. b d d 5 e 2 2 2 2 2 e 5 5 5 1 1 5 1 1 4 d 5 4 d 4 e e . . . . 
-. b 5 5 d 2 e 2 2 2 e 3 5 1 1 4 4 d 1 d 5 5 e e e e . . . . . . 
-b d 5 d d d 2 e e e 3 d 4 4 4 5 5 5 d d 4 e . . . . . . . . . . 
-b 5 5 5 1 1 5 d d d d 5 5 d 5 4 4 e e d e . . . . . . . . . . . 
-4 5 1 1 d 4 4 4 d 5 5 4 4 e e . . . 4 d e . . . . . . . . . . . 
-4 1 d 4 5 5 5 d 4 e e . . . . . . . . 4 4 . . . . . . . . . . . 
-4 1 5 5 4 e e d e . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . b d 5 5 5 5 5 5 5 5 d 4 b b 4 4 4 4 4 4 e e . . 
+. . . . . . . . b 5 5 5 5 5 5 5 5 5 5 5 b b 4 b 4 4 4 4 4 e e . 
+. . . . . . . b d 5 5 5 5 5 5 5 5 5 5 5 5 4 e b b 4 4 4 4 4 e b 
+. . . . . . . 4 3 5 5 5 5 5 5 5 5 5 5 5 5 d 4 e e b 4 4 b 4 e e 
+. . . . . . b 4 5 5 5 5 5 5 5 5 5 5 5 5 5 5 d 4 e e b b b 4 b e 
+. . . . . . b 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 d b 4 e b b b b e 
+. . . . . b d 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 d d 4 b b b b e 
+. . . . . b 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 d d d 4 e b e e 
+. . . . b d 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 4 4 e e e 
+. . . . 6 6 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 4 d e e e 
+. . . 6 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 4 d 4 e e 
+. . . 4 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 d 4 e e e 
+. . b 4 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 d d e e e . 
+. . 4 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 4 e e e . . 
+. b 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 4 5 4 e e . . . . 
+. b 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 e e e e . . . . . . 
+b d 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 4 e . . . . . . . . . . 
+b 5 5 5 5 5 5 5 5 5 5 5 5 5 5 4 4 e e 5 e . . . . . . . . . . . 
+4 5 5 5 5 5 5 5 5 5 5 4 4 e e . . . 4 5 e . . . . . . . . . . . 
+4 5 5 5 5 5 5 5 4 e e . . . . . . . . 4 4 . . . . . . . . . . . 
+4 5 5 5 4 e e 5 e . . . . . . . . . . . . . . . . . . . . . . . 
 4 4 e e . . . 4 e . . . . . . . . . . . . . . . . . . . . . . . 
 `, SpriteKind.Player)
 let favoritefood2 = sprites.create(img`
 . . . . . . b b b b a a . . . . 
-. . . . b b d d d 3 3 3 a a . . 
-. . . b d d d 3 3 3 3 3 3 a a . 
-. . b d d 3 3 3 3 3 3 3 3 3 a . 
-. b 3 d 3 3 3 3 3 b 3 3 3 3 a b 
-. b 3 3 3 3 3 a a 3 3 3 3 3 a b 
-b 3 3 3 3 3 a a 3 3 3 3 d a 4 b 
-b 3 3 3 3 b a 3 3 3 3 3 d a 4 b 
-b 3 3 3 3 3 3 3 3 3 3 d a 4 4 e 
-a 3 3 3 3 3 3 3 3 3 d a 4 4 4 e 
-a 3 3 3 3 3 3 3 d d a 4 4 4 e . 
-a a 3 3 3 d d d a a 4 4 4 e e . 
+. . . . b b d d d e e e a a . . 
+. . . b d d d e e e e e e a a . 
+. . b d d e e e e e e e e e a . 
+. b e d e e e e e b e e e e a b 
+. b e e e e e a a e e e e e a b 
+b e e e e e a a e e e e d a 4 b 
+b e e e e b a e e e e e d a 4 b 
+b e e e e e e e e e e d a 4 4 e 
+a e e e e e e e e e d a 4 4 4 e 
+a e e e e e e e d d a 4 4 4 e . 
+a a e e e d d d a a 4 4 4 e e . 
 . e a a a a a a 4 4 4 4 e e . . 
 . . e e b b 4 4 4 4 b e e . . . 
 . . . e e e e e e e e . . . . . 
